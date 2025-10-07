8ff60f1f2197b99b020c8977504506c3978171ff
charliewang123 <charliewang123@g.ucla.edu>
2025-10-07 08:51:20 -0700

    Change back

--- R/decompose.R
+++ R/decompose.R
@@ -8,73 +8,52 @@
 #'
 #' @export
 decomposition_step = function(exp_mat_filename, data_dir){
-  if(!dir.exists(data_dir)) dir.create(data_dir)
-  
-  exp_mat = data.table::fread(file = exp_mat_filename, sep = '\t', data.table = FALSE)
-  
-  design = sapply(1:nrow(exp_mat), function(i) unlist(strsplit(exp_mat[,1][i], split = " - "))[1])
-  context_names = sapply(1:nrow(exp_mat), function(i) unlist(strsplit(exp_mat[,1][i], split = " - "))[2])
-  contexts = unique(context_names)
-  
-  print("Decomposing data")
-  rownames(exp_mat) = exp_mat[,1]
-  exp_mat = exp_mat[, -1]
-  
-  cat(sprintf("There are %s samples and %s genes. The max number of missing samples for a gene is %s. The max number of missing genes for a sample is %s.\n",
-              nrow(exp_mat), ncol(exp_mat), max(colSums(is.na(exp_mat))), max(rowSums(is.na(exp_mat)))))
-  
-  dec_exp_all = decompose(X = exp_mat, design = design)
-  bexp_all = dec_exp_all$Xb
-  wexp_all = dec_exp_all$Xw
-  bexp_all[is.nan(bexp_all)] = NA
-  wexp_all[is.nan(wexp_all)] = NA
-  
-  cat(sprintf("Between-individual matrix: %s individuals, %s genes. Max missing per gene: %s. Max missing per sample: %s.\n",
-              nrow(bexp_all), ncol(bexp_all), max(colSums(is.na(bexp_all))), max(rowSums(is.na(bexp_all)))))
-  cat(sprintf("Within-individual matrix: %s samples, %s genes. Max missing per gene: %s. Max missing per sample: %s.\n",
-              nrow(wexp_all), ncol(wexp_all), max(colSums(is.na(wexp_all))), max(rowSums(is.na(wexp_all)))))
-  
-  # Function to clean expression files
-  clean_expr_file <- function(f, n_individuals = 300) {
-    lines <- readLines(f)
-    header <- strsplit(lines[1], "\t")[[1]]
-    if (header[1] == "") header <- header[-1]
-    header <- header[header != "V301"]
-    correct_header <- c("geneID", paste0("ind", 1:n_individuals))
-    rows <- strsplit(lines[-1], "\t")
-    maxlen <- max(sapply(rows, length))
-    rows <- lapply(rows, function(x) { length(x) <- maxlen; x })
-    mat <- do.call(rbind, rows)
-    df <- as.data.frame(mat, stringsAsFactors = FALSE)
-    colnames(df) <- correct_header
-    for (j in 2:ncol(df)) df[[j]] <- suppressWarnings(as.numeric(df[[j]]))
-    rownames(df) <- df$geneID
-    df <- df[, -1]
-    write.table(df, file = f, sep = "\t", row.names = TRUE, col.names = NA, quote = FALSE)
-  }
-  
-  # Save shared expression matrix
-  print("Saving between-individuals variation matrix")
-  shared_file <- paste0(data_dir, "context_shared_expression.txt")
-  fwrite(
-    x = data.table::data.table(t(bexp_all), keep.rownames = TRUE) %>%
-      { setnames(., old = "rn", new = "geneID")[] },
-    file = shared_file, quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t"
-  )
-  clean_expr_file(shared_file)
-  
-  # Save context-specific expression matrices
-  print("Saving within-individuals variation matrix for context:")
-  for (i in seq_along(contexts)) {
-    print(contexts[i])
-    wexp_t = wexp_all[grep(pattern = paste0(contexts[i], "$"), rownames(wexp_all)), ]
-    rownames(wexp_t) = gsub(pattern = paste0(" - ", contexts[i]), replacement = "", x = rownames(wexp_t))
-    context_file <- paste0(data_dir, contexts[i], "_specific_expression.txt")
-    fwrite(
-      x = data.table::data.table(t(wexp_t), keep.rownames = TRUE) %>%
-        { setnames(., old = "rn", new = "geneID")[] },
-      file = context_file, quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t"
-    )
-    clean_expr_file(context_file)
-  }
+if(!dir.exists(data_dir)) dir.create(data_dir)
+#%%%%%%%%%%%%%%% Read expression matrix, genes in columns, samples in rows.
+#exp_mat=read.table(file = paste0(data_dir,exp_mat_filename), sep = '\t')
+exp_mat=data.table::fread(file = exp_mat_filename, sep = '\t', data.table = F)
+
+#%%%%%%%%%%%%%%% Sample and context names
+design=sapply(1:nrow(exp_mat), function(i) unlist(strsplit(exp_mat[,1][i], split = " - "))[1])
+context_names=sapply(1:nrow(exp_mat), function(i) unlist(strsplit(exp_mat[,1][i], split = " - "))[2])
+contexts=unique(context_names)
+
+#%%%%%%%%%%%%%%% Decompose expression into homogeneous and heterogeneous context expression
+print("Decomposing data")
+rownames(exp_mat) = exp_mat[,1]
+exp_mat = exp_mat[,-1]
+
+#%%%%%%%%%%%%%%% Print number of genes and samples
+string1 = sprintf("There are %s samples and %s genes. The max number of missing samples for a gene is  %s. The max number of missing genes for a sample is  %s. \n", nrow(exp_mat), ncol(exp_mat),max(colSums(is.na(exp_mat))),max(rowSums(is.na(exp_mat))))
+cat(string1)
+
+dec_exp_all=decompose(X = exp_mat, design = design)
+bexp_all=dec_exp_all$Xb
+wexp_all=dec_exp_all$Xw
+bexp_all[is.nan(bexp_all)]=NA
+wexp_all[is.nan(wexp_all)]=NA
+
+string2 = sprintf("Between individual matrix: There are %s individuals and %s genes. The max number of missing samples for a gene is  %s. The max number of missing genes for a sample is  %s. \n", nrow(bexp_all), ncol(bexp_all),max(colSums(is.na(bexp_all))),max(rowSums(is.na(bexp_all))))
+cat(string2)
+
+string3 = sprintf("Within individual matrix: There are %s samples and %s genes. The max number of missing samples for a gene is  %s. The max number of missing genes for a sample is  %s. \n", nrow(wexp_all), ncol(wexp_all),max(colSums(is.na(wexp_all))),max(rowSums(is.na(wexp_all))))
+cat(string3)
+
+#%%%%%%%%%%%%%%% Save decomposed expression files 
+print("Finished decomposition, saving files")
+
+print("Saving between-individuals variation matrix")
+fwrite(x = data.table::data.table(t(bexp_all),keep.rownames = T) %>% {setnames(., old = "rn", new = "geneID")[]},
+       file = paste0(data_dir,"context_shared_expression.txt"), quote = F, row.names = F,
+       col.names = T, append = F, sep = '\t')
+
+print("Saving within-individuals variation matrix for context: ")
+for(i in 1:length(contexts)){
+  print(contexts[i])
+  wexp_t = wexp_all[grep(pattern = paste0(contexts[i],"$"), rownames(wexp_all)),]
+  rownames(wexp_t)=gsub(pattern = paste0(" - ",contexts[i]), replacement = "", x = rownames(wexp_t))
+  fwrite(x = data.table::data.table(t(wexp_t),keep.rownames = T) %>% {setnames(., old = "rn", new = "geneID")[]},
+         file = paste0(data_dir,contexts[i],"_specific_expression.txt"),quote = F, row.names = F,
+         col.names = T, append = F, sep = '\t')
+}
 }
--- R/simulate_data.R
+++ R/simulate_data.R
@@ -24,93 +24,64 @@
 #'
 #' @export
 simulate_data = function(data_dir, N = 300, n_genes = 100, n_snps_per_gene = 1000,
-                         n_contexts = 10, maf = 0.2, w_corr = 0.2,
-                         v_e = 1, missing = 0,
-                         hsq = rep(0,n_contexts),
-                         mus = rep(0, n_contexts)) {
-
-  clean_expr_file <- function(f) {
-    lines <- readLines(f)
-    header <- strsplit(lines[1], "\t")[[1]]
-    if (header[1] == "") header <- header[-1]
-    header <- header[header != "V301"]
-    correct_header <- c("geneID", paste0("ind", 1:N))
-    
-    rows <- strsplit(lines[-1], "\t")
-    maxlen <- max(sapply(rows, length))
-    rows <- lapply(rows, function(x) { length(x) <- maxlen; x })
-    mat <- do.call(rbind, rows)
-    df <- as.data.frame(mat, stringsAsFactors = FALSE)
-    colnames(df) <- correct_header
-    for (j in 2:ncol(df)) df[[j]] <- suppressWarnings(as.numeric(df[[j]]))
-    rownames(df) <- df$geneID
-    df <- df[, -1]
-    write.table(df, file = f, sep = "\t", row.names = TRUE, col.names = NA, quote = FALSE)
-  }
-
+                        n_contexts = 10, maf = 0.2, w_corr = 0.2,
+                        v_e = 1, missing = 0,
+                        hsq = rep(0,n_contexts),
+                        mus = rep(0, n_contexts)) {
   if (!dir.exists(data_dir)) dir.create(data_dir)
   sigma = matrix(w_corr, nrow = n_contexts, ncol = n_contexts)
   diag(sigma) = v_e
-  genos = sapply(1:(n_snps_per_gene * n_genes), function(X) rbinom(N, 2, maf))
+  genos = sapply(X = 1:(n_snps_per_gene * n_genes), FUN = function(X) rbinom(N, 2, maf))
   colnames(genos) = paste0("snp", 1:(n_snps_per_gene * n_genes))
   rownames(genos) = paste0("ind", 1:N)
-  write.table(t(data.table(genos, keep.rownames = TRUE) %>%
-                  rename(snpid = rn)),
-              file = paste0(data_dir, "SNPs.txt"), quote = FALSE, sep = "\t", row.names = TRUE, col.names = FALSE)
+  write.table(x = t(data.table(genos, keep.rownames = T) %>%
+                      rename(snpid = rn)), file = paste0(data_dir, "SNPs.txt"), quote = F, sep = "\t", row.names = T, col.names = F)
   print("Finished simulating and saving genotypes")
-  
   snp_loc = data.frame(snpid = colnames(genos), chr = "chr1",
-                       pos = rep(seq(1, n_genes * 1e9, by = 1e9), each = n_snps_per_gene),
-                       matrix(1, nrow = ncol(genos), ncol = n_contexts,
+                       pos = rep(x = seq(1, n_genes * 1e+09, by = 1e+09), each = n_snps_per_gene),
+                       matrix(data = 1, nrow = ncol(genos), ncol = n_contexts,
                               dimnames = list(NULL, paste0("context", 1:n_contexts))))
-  write.table(snp_loc, file = paste0(data_dir, "snpsloc.txt"), quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(x = snp_loc, file = paste0(data_dir,"snpsloc.txt"), quote = F, sep = "\t", row.names = F,
+              col.names = T)
   print("Finished saving snp location file")
-  
   gene_loc = data.frame(geneid = paste0("gene", 1:n_genes),
-                        chr = "chr1", s1 = seq(1, n_genes * 1e9, by = 1e9),
-                        s2 = seq(1, n_genes * 1e9, by = 1e9) + 1000,
-                        matrix(1, nrow = n_genes, ncol = n_contexts,
-                               dimnames = list(NULL, paste0("context", 1:n_contexts))))
-  write.table(gene_loc, file = paste0(data_dir, "geneloc.txt"), quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+                        chr = "chr1", s1 = seq(1, n_genes * 1e+09, by = 1e+09),
+                        s2 = seq(1, n_genes * 1e+09, by = 1e+09) + 1000, matrix(data = 1,
+                                                                                nrow = n_genes, 
+                                                                                ncol = n_contexts, 
+                                                                                dimnames = list(NULL,
+                                                                                paste0("context", 1:n_contexts))))
+  write.table(x = gene_loc, file = paste0(data_dir, "geneloc.txt"), quote = F, sep = "\t", row.names = F,
+              col.names = T)
   print("Finished saving gene location file")
-  
-  genos_with_effect = genos[, seq(from = 1, to = (n_snps_per_gene * n_genes), by = n_snps_per_gene)]
-  exp_mat = expand.grid(iid = paste0("ind", 1:N), context = paste0("context", 1:n_contexts))
-  which_context = rep_len(1:n_contexts, length.out = n_genes)
-  
+  genos_with_effect = genos[, seq(from = 1, to = (n_snps_per_gene *
+                                                    n_genes), by = n_snps_per_gene)]
+  exp_mat = expand.grid(iid = paste0("ind", 1:N), context = paste0("context",
+                                                                   1:n_contexts))
+  which_context = rep_len(x = 1:n_contexts, length.out = n_genes)
   for (i in 1:n_genes) {
-    betas = sqrt((hsq * v_e) / ((1 - hsq) * var(genos_with_effect[, i])))
-    Y = matrix(0, nrow = N, ncol = n_contexts,
-               dimnames = list(paste0("ind", 1:N), paste0("context", 1:n_contexts)))
-    for (j in 1:n_contexts) {
-      Y[, j] = mus[j] + genos_with_effect[, i] * betas[j]
-    }
-    for (j in 1:N) {
-      Y[j, ] = Y[j, ] + rmvnorm(1, rep(0, n_contexts), sigma)
-    }
-    data_mat = melt(data.table(Y, keep.rownames = TRUE), id.vars = "rn")
+    betas = sqrt((hsq * v_e)/((1 - hsq) * var(genos_with_effect[, i])))
+    Y = matrix(0, nrow = N, ncol = n_contexts, dimnames = list(paste0("ind", 1:N), paste0("context", 1:n_contexts)))
+    for (j in 1:n_contexts) Y[, j] = mus[j] + genos_with_effect[, i] * betas[j]
+    for (j in 1:N) Y[j, ] = Y[j, ] + rmvnorm(1, rep(0, n_contexts), sigma)
+    data_mat = melt(data = data.table(Y, keep.rownames = T),
+                    id.vars = "rn")
     colnames(data_mat) = c("iid", "context", paste0("gene", i))
-    exp_mat = merge(exp_mat, data_mat)
+    exp_mat = merge(x = exp_mat, y = data_mat)
   }
-  
   rownames(exp_mat) = paste(exp_mat$iid, exp_mat$context, sep = " - ")
-  exp_mat = cbind(data.frame(design = paste(exp_mat$iid, exp_mat$context, sep = " - ")), exp_mat)
+  exp_mat = cbind(data.frame(design = paste(exp_mat$iid, exp_mat$context,  sep = " - ")), exp_mat)
   total_elements = prod(dim(exp_mat[, -c(1:3)]))
   missing_elements = floor(missing * total_elements)
   missing_indices = sample(1:total_elements, missing_elements)
   identifiers = exp_mat[, 1:3]
   exp_mat_missing = as.vector(as.matrix(exp_mat[, -c(1:3)]))
   exp_mat_missing[missing_indices] = NA
-  exp_mat_missing = matrix(exp_mat_missing, nrow = nrow(exp_mat), ncol = ncol(exp_mat) - 3)
+  exp_mat_missing = matrix(exp_mat_missing, nrow = nrow(exp_mat),  ncol = ncol(exp_mat) - 3)
   exp_mat_missing = as.data.frame(exp_mat_missing)
   exp_mat_missing = cbind(identifiers, exp_mat_missing)
   colnames(exp_mat_missing) = colnames(exp_mat)
-
-  expr_file_path <- file.path(data_dir, "expression.txt")
-  write.table(exp_mat_missing[, -c(2:3)], file = expr_file_path,
-              quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
+  write.table(x = exp_mat_missing[, -c(2:3)], file = paste0(data_dir, "expression.txt"), quote = F,
+              sep = "\t", row.names = F, col.names = T)
   print("Finished simulating and saving expression file")
-
-  clean_expr_file(expr_file_path)
-  print("Cleaned expression file for downstream compatibility")
 }
